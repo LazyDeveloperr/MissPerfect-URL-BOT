0c73c590cc305b8d0100b6ab5cc3ac7980676634
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-08 17:26:35 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/LazyDeveloperr/MissPerfectURLBOT /MissPerfectURLBOT
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
